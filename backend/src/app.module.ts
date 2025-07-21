962f4318c9b247fcfe72f30075f4b9d5c4a8a8f9
Dima <skydmytro@gmail.com>
2025-07-21 15:36:47 +0200

    chore(deploy): add Docker Compose for app deployment

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { LoggerModule } from 'nestjs-pino';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
     LoggerModule.forRoot({
       pinoHttp: {
         transport: {
